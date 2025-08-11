eb3fe6300b607be8ffec5206c203da674a16f3ca
Aman Kumar <ak93896897@gmail.com>
2025-08-11 19:11:34 +0530

    Complete Node js

---
+++ 8 Node Js and Express Js/5 Tools and NPM copy/RequestHandler.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const { URLSearchParams } = require("url");
+
+const RequestHandler = (req, res) => {
+  console.log("The request recieved: ", req.url, req.method);
+  res.setHeader("content-type", "text/html");
+
+  if (req.url === "/") {
+    res.write(`
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <title>Myntra</title>
+        </head>
+        <body>
+          <h1>Welcome to Catch data</h1>
+          <form action="/buy-products" method="POST">
+            <input type="text" placeholder="Enter your product name" name="product"/>
+            <input type="text" placeholder="Enter your buget" name="buget"/>
+            <input type="submit" />
+          </form>
+        </body>
+      </html>
+    `);
+    res.end();
+  } else if (req.url === "/buy-products") {
+    console.log("Yes, I am inside the buy product");
+
+    const buffer = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      buffer.push(chunk);
+    });
+
+    req.on("end", () => {
+      const body = Buffer.concat(buffer).toString();
+      const urlParams = new URLSearchParams(body);
+      console.log("This is your url Params", urlParams);
+      const bodyJSON = {};
+      for (const [key, value] of urlParams.entries()) {
+        bodyJSON[key] = value;
+      }
+      fs.writeFileSync("buy.txt", JSON.stringify(bodyJSON), () => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/product");
+        res.end();
+        console.log("Response Sending");
+      });
+    });
+  } else if (req.url === "/product") {
+    res.write(`
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>Product</title>
+      </head>
+      <body style="background: #555;">
+        <h1>Welcome to Product list server</h1>
+      </body>
+      </html>
+      `);
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write(`
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>404</title>
+      </head>
+      <body>
+        <h1>404 Not found</h1>
+      </body>
+      </html>
+    `);
+    res.end();
+  }
+};
+
+module.exports = RequestHandler;
