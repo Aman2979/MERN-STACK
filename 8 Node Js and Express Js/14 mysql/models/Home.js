f49df125c8ac64c2349aa20d1eaba7de0513ee0e
Aman Kumar <ak93896897@gmail.com>
2025-08-22 21:27:47 +0530

    Some more DB

---
+++ 8 Node Js and Express Js/14 mysql/models/Home.js
@@ -0,0 +1,24 @@
+const Favourites = require("./Favourites");
+const airbnbDb = require("../util/database-util");
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
+
+  save(callback) {
+    
+  }
+
+  static fetchAll() {
+    return airbnbDb.execute("SELECT * FROM homes")
+  }
+
+  static findById(homeId, callback) {}
+
+  static deletById(homeId, callback) {}
+};
