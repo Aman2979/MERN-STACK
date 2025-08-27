21b37d600fd57d9bb85ff25650a088982bb0eea8
Aman Kumar <ak93896897@gmail.com>
2025-08-27 20:43:44 +0530

    Done

---
+++ 8 Node Js and Express Js/15-mongodb/models/Home.js
@@ -0,0 +1,31 @@
+const { getDb } = require("../util/database-util");
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl, description) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+    this.description = description;
+  }
+
+  save() {
+    const db = getDb()
+    return db.collection("homes").insertOne(this).then(result =>{
+      console.log(result)
+    })
+  }
+
+  static fetchAll() {
+
+  }
+
+  static findById(homeId) {
+
+  }
+
+  static deletById(homeId, callback) {
+
+  }
+};
